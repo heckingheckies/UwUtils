ff3c5a748fd9b0f91a6d2da6613f2df3c71dc5df
tatsu <mrscy22.feather@gmail.com>
2024-01-07 16:57:34 +0100

    h

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   kotlin("jvm") version "1.9.0"
   application
-  id("com.github.johnrengelman.shadow") version "7.1.2"
   java
   id("maven-publish")
 }
@@ -15,17 +14,12 @@ repositories {
   mavenCentral()
   maven("https://oss.sonatype.org/content/groups/public/")
   maven("https://repo.papermc.io/repository/maven-public/")
-  maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
   maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
   implementation(kotlin("stdlib"))
   compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
-
-  implementation("com.github.Revxrsal.Lamp:common:3.1.7")
-  implementation("com.github.Revxrsal.Lamp:bukkit:3.1.7")
-  implementation("com.moandjiezana.toml:toml4j:0.7.2")
 }
 
 tasks.test { useJUnitPlatform() }
@@ -43,4 +37,4 @@ publishing {
 
 tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "17" }
 
-application { mainClass.set("MinethologyKt") }
+application { mainClass.set("UwUtilsKt") }
