3a6efefbf9eaf2507afd94f6ed9f5f082629e277
tatsu <mrscy22.feather@gmail.com>
2024-01-07 15:18:22 +0100

    fuck

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ tasks {
 
 tasks.test { useJUnitPlatform() }
 
-tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "17" }
+tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "1.8" }
 
 application { mainClass.set("CutePluginKt") }
 
